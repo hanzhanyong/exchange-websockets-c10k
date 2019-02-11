d030997338dec75f4f685b43c4fbfb9694b49d79
zhanyong168 <zhanyong168@sina.com>
2019-02-11 23:57:49 +0800

    amqp client Test

--- utils/amqpClient.py
+++ utils/amqpClient.py
@@ -19,7 +19,7 @@ class amqpClient(object):
         self.exchangeName = None
         self.queueName = None
 
-    async def open(self, exchangeName='', queueName='hello'):
+    async def open(self, exchangeName='', queueName='hello', callback=None):
         try:
             self.transport, self.protocol = await aioamqp.connect(
                 host=self.config["host"],
@@ -29,7 +29,17 @@ class amqpClient(object):
                 virtualhost=self.config["vhost"],
                 loop=self.loop)
             self.channel = await self.protocol.channel()
+
+            # await self.channel.queue_declare(
+            #     queue_name=queueName, durable=True)
             await self.channel.queue_declare(queue_name=queueName)
+
+            if callback:
+                await self.channel.basic_qos(
+                    prefetch_count=1, prefetch_size=0, connection_global=False)
+                await self.channel.basic_consume(
+                    callback, queue_name=queueName)
+
             self.exchangeName = exchangeName
             self.queueName = queueName
         except Exception as ex:
@@ -45,31 +55,67 @@ class amqpClient(object):
         self.transport.close()
 
     async def sendMessage(self, msg):
-        await channel.basic_publish(
-            payload=msg, exchange_name='', routing_key='hello')
+        await self.channel.basic_publish(
+            payload=msg,
+            exchange_name=self.exchangeName,
+            routing_key=self.queueName)
 
-    async def receiveMessage(self, msg):
-        print(msg)
+    # async def receiveMessage(self, channel, body, envelope, properties):
+    #     print(" [x] Received %r" % body)
+    #     await asyncio.sleep(body.count(b'.'))
 
 
 """
 ---------------------TEST----------------------------
 """
 
-from .settings import settings
+sConfig = {
+    'vhost': '/',
+    'host': "127.0.0.1",
+    "port": 5672,
+    "user": "guest",
+    "password": "guest"
+}
 
 
 async def exampleSend(loop):
-    sConfig = settings()
-    amqp = amqpClient(sConfig.rabbitmq, loop)
-    opened = await amqp.open()
-    if opened is True:
-        while 
-        amqp.sendMessage("test{}".format(time.time()))
 
-        amqp.close()
+    amqp = amqpClient(sConfig, loop)
+    opened = await amqp.open()
+    # print(opened)
+    if opened is True:
+        numIndex = 0
+        while True:
+            await amqp.sendMessage("test time {}".format(time.time()))
+            numIndex += 1
+            if numIndex == 1000:
+                break
+
+        await amqp.close()
+
+
+async def mycallback(channel, body, envelope, properties):
+    await asyncio.sleep(0.01)
+    print(" [x] Received %r" % body)
+    # print(" [x] Done")
+    await channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
+
+
+async def worker(loop):
+    amqp = amqpClient(sConfig, loop)
+    opened = await amqp.open(callback=mycallback)
+    # print(opened)
+    if opened is True:
+        print("opened is ok")
+    else:
+        print("opened is error")
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(exampleSend(loop))
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(exampleSend(loop))
+    # event_loop.run_forever()
+
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(worker(event_loop))
+    event_loop.run_forever()
